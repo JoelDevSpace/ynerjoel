4a02815fd3d42ff7f65d53a0455773b9a246168d
JoelDevSpace <joel_jmg@hotmail.com>
2025-07-10 21:17:34 +0200

    PROJET Mise en place des TODOS

--- resources/js/pages/Admin/Admin.vue
+++ resources/js/pages/Admin/Admin.vue
@@ -19,13 +19,32 @@ const breadcrumbs: BreadcrumbItem[] = [
         <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                    <p>TODO Administration</p>
+                    <ul>
+                        <li>CRUD pour les permissions</li>
+                        <li>CRUD pour les types de clients</li>
+                        <li>CRUD pour les types de personnes morales</li>
+                        <li>CRUD pour les types de fiche FOS</li>
+                        <li>CRUD pour les secteurs des fiche FOS</li>
+                    </ul>
                 </div>
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                    <p>TODO hors Administration</p>
+                    <ul>
+                        <li>CRUD fiches</li>
+                        <li>CRUD Bénéficiaires</li>
+                        <li>CRUD Installateurs</li>
+                        <li>CRUD contrats Installateurs</li>
+                        <li>Dossier</li>
+                    </ul>
                 </div>
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                    <p>TODO Design</p>
+                    <ul>
+                        <li class="bold text-red-700">Pagination</li>
+                        <li>tester les datatables</li>
+                        <li>tester les onglets</li>
+                    </ul>
                 </div>
             </div>
             <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
